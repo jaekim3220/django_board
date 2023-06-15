a51c74cce33d0b2f1cca836e5d8baafa7ca13feb
jaekim3220 <jehyunkim9@gmail.com>
2023-06-15 17:38:34 +0900

    간단한 웹사이트 생성 이메일, 작성자 추가

--- board_project/board_main/models.py
+++ board_project/board_main/models.py
@@ -1,34 +1,31 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
-# models.py이 클래스와 db의 테이블과 sync를 맞춰 테이블(컬럼정보) 자동생성
+# models.py의 클래스와 DB의 table과 sync를 맞춰 테이블(컬럼정보) 자동생성.
 
-# 클래스 이름 = 테이블 이름, 클래스 변수 = 컬럼명
+# 클래스명 = 테이블명, 변수=컬럼명
 class Test(models.Model):
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
     password = models.CharField(max_length=30)
 
-
-'''author 실전 모델 생성 '''
-class Author(models.Model): #Author을 모델로 사용하겠다
+class Author(models.Model):
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=30)
-    # db 설정에 default timestamp가 걸리는 것이 아닌, 장고가 현재 시간을 db에 입력
-    created_at = models.DateTimeField(auto_now_add=True) # get current timestamp로 생성시간
-    updated_at = models.DateTimeField(auto_now=True) # 수정시간
+    # DB설정에 default timestamp가 걸리는 것이아닌, 장고가 현재시간을 db에 insert
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-'''post 실전 모델 생성 '''
-class Post(models.Model): #Author을 모델로 사용하겠다
-    title = models.CharField(max_length=100) #varchar(100)
-    contents = models.TextField(max_length=50) # 메모리 save
-    created_at = models.DateTimeField(auto_now_add=True) # get current timestamp로 생성시간
-    updated_at = models.DateTimeField(auto_now=True) # 수정시간
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    contents = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # DB에는 fk를 설정한 변수명에 _id 가 붙게 된다.
+    # on_delete = models.CASCADE, 
+    # db에 author_id는 파이썬에서는 author객체와 같은것.
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL ,null=True, related_name='posts')
+
     
-    # 여기부터 DB와 class의 차이
-    # db에는 fk를 설정한 변수명에 _id가 붙게 된다
-    # Post에서 author 객체 정보를 조회, 
-    # on_delete = models.CASCADE
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True) # 상단의 Author에 FK를 걸어준다
-
--- board_project/board_main/views.py
+++ board_project/board_main/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render, redirect
-# from django.http import JsonResponse, HttpResponse
-from .models import Author, Post #Test
+from .models import Author, Post
+from django.http import HttpResponse, HttpResponseNotFound
 
-# # Create your views here.
-
-# home 화면을 구성하는 html생성, requests는 templates 까지 생략
 def home(request):
     return render(request, 'home.html')
-
-
-# urls.py의 path('authors', views.author_list),
-def author_list(request):
-    authors = Author.objects.all() # Author의 모든 데이터 추출
-    return render(request, 'author/author_list.html', {'authors':authors})
-# 'author/author_list.html'은 author 폴더 내부의 author_list.html이므로 그대로
-# 동작 안하면 캐시 삭제
 
 
 def author_new(request):
@@ -22,96 +11,77 @@ def author_new(request):
         my_name = request.POST['my_name']
         my_email = request.POST['my_email']
         my_password = request.POST['my_pw']
-        # DB에 insert -> save 함수 사용
-        # DB의 테이블과 sync가 맞는 Test 클래스에서 객체를 생성해 save
-        a1 = Author() #객체 생성은 python_basic의 class_statements.py 참고
-        a1.name = my_name #t1 객체에 my_name 세팅
+        a1 = Author()
+        a1.name = my_name
         a1.email = my_email
         a1.password = my_password
         a1.save()
-        return redirect('/') # localhost:8000/(home)으로 가라
+        return redirect('/')
     else:
-        return render(request, 'author/author_new.html') 
-    # author/author_new.html으로 return
+        return render(request, 'author/author_new.html')
+    
+def author_list(request):
+    # authors = Author.objects.all()
+    authors = Author.objects.filter().order_by('-created_at')
+    return render(request, 'author/author_list.html', {'authors':authors})
 
-
-def author_detail(request,my_id):
-    author = Author.objects.get(id=my_id) # Author의 모든 데이터 추출
+def author_detail(request, my_id):
+    author = Author.objects.get(id=my_id)
     return render(request, 'author/author_detail.html', {'author':author})
 
-
-def author_update(request,my_id):
-    author = Author.objects.get(id=my_id) # Author의 모든 데이터 추출
+def author_update(request, my_id):
+    author = Author.objects.get(id=my_id)
     if request.method == 'POST':
         my_name = request.POST['my_name']
         my_email = request.POST['my_email']
         my_password = request.POST['my_pw']
-        # 아래는 정석으로는 if문으로 작성
-        author.name = my_name 
+        author.name = my_name
         author.email = my_email
         author.password = my_password
-        author.save() 
-        # save : 신규 객체 save 시 insert, 기존 객체 save 시 update
-        return redirect('/') # localhost:8000/으로 가라(home으로)
+        author.save()
+        return redirect('/')
     else:
         return render(request, 'author/author_update.html', {'author':author})
-    # {'author':author} : 이미 조회한 데이터를 던져 줌
 
-
-
-
-# urls.py의 path('authors', views.author_list),
 def post_list(request):
-    # order_by하고 -컬럼명은 내림차순 created_at
+    # order_by하고 -컬럼명 이렇게 주면 내림차순정렬
     posts = Post.objects.filter().order_by('-created_at')
-    # posts = Post.objects.all()
-    # 다건 조회는 all 외에도 filter가 존재
-    return render(request, 'post/post_list.html', {'posts':posts})
-# 'post/post_list.html'은 post 폴더 내부의 post/post_list.html이므로 그대로
-
+    return render(request, 'post/post_list.html', {"posts":posts})
 
 def post_new(request):
     if request.method == 'POST':
         my_title = request.POST['my_title']
         my_contents = request.POST['my_contents']
-
         my_email = request.POST['my_email']
-        # my_writter = request.POST['my_writter']
-        # DB에 insert -> save 함수 사용
-        # DB의 테이블과 sync가 맞는 Test 클래스에서 객체를 생성해 save
-        p1 = Post() #객체 생성은 python_basic의 class_statements.py 참고
-        p1.title = my_title #t1 객체에 my_name 세팅
+        p1 = Post()
+        if my_email:
+            try:
+                a1 = Author.objects.get(email = my_email)
+                # 장고에서 a1객체에서 id값만 빼서, db에 저장할때는 author_id에 id값을 저장
+                p1.author = a1 #{id:1, name:"hong", email:"hong@naver" ...}
+            except Author.DoesNotExist:
+                # HttpResponse는 200 정상
+                return HttpResponseNotFound("존재하지 않는 이메일입니다.")
+        p1.title = my_title
         p1.contents = my_contents
-        
-        # p1.email = my_email
-        # p1.author.name = my_writter
-        # p1.writter = my_writter
         p1.save()
-        return redirect('/') # localhost:8000/(home)으로 가라
+        return redirect('/')
     else:
-        return render(request, 'post/post_new.html') 
+        return render(request, 'post/post_new.html')
 
 
-def post_detail(request,my_id):
-    post = Post.objects.get(id=my_id) # Post의 모든 데이터 추출
-    return render(request, 'post/post_detail.html', {'post':post})
+def post_detail(request, my_id):
+    post = Post.objects.get(id=my_id)
+    return render(request, 'post/post_detail.html', {'post': post})
 
-
-def post_update(request,my_id):
-    post = Post.objects.get(id=my_id) # Author의 모든 데이터 추출
+def post_update(request, my_id):
+    post = Post.objects.get(id=my_id)
     if request.method == 'POST':
         my_title = request.POST['my_title']
         my_contents = request.POST['my_contents']
-        my_email = request.POST['my_email']
-        # my_writter = request.POST['my_writter']
-        # 아래는 정석으로는 if문으로 작성
-        post.title = my_title 
+        post.title = my_title
         post.contents = my_contents
-        # post.writter = my_writter
-        
-        post.save() 
-        # save : 신규 객체 save 시 insert, 기존 객체 save 시 update
-        return redirect('/') # localhost:8000/으로 가라(home으로)
+        post.save()
+        return redirect('/')
     else:
         return render(request, 'post/post_update.html', {'post':post})
-    # {'post':post} : 이미 조회한 데이터를 던져 줌
